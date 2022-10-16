219aa40c011fe53fae60e22a60f57190919cb518
SU-T <guitaristpeet@gmail.com>
2022-10-16 13:48:54 -0700

    select more h1 el and put animation timeline inside

--- header.js
+++ header.js
@@ -1,26 +1,30 @@
-const header = document.querySelector('h1');
-const logo = header.querySelector('span');
-const logoWidth = logo.clientWidth;
+const headers = document.querySelectorAll('header h1, section h1');
 
-const h1Tl = gsap.timeline({repeat: -1});
-h1Tl
-    .set(header, {x: logoWidth * 1})
-    .to(header, {
-        x: logoWidth * -1,
-        ease: 'linear',
-        duration: 3,
-    })
+headers.forEach((header) => {
+    const logo = header.querySelector('span');
+    const logoWidth = logo.clientWidth;
+
+    const h1Tl = gsap.timeline({repeat: -1});
+    h1Tl
+        .set(header, {x: logoWidth * 1})
+        .to(header, {
+            x: logoWidth * -1,
+            ease: 'linear',
+            duration: 3,
+        })
+    
+    const logoTl = gsap.timeline({repeat: -1});
+    logoTl
+        .set(logo, {
+            fontVariationSettings: '"wght" 130, "slnt" 45',
+        })
+        .to(logo, {
+            fontVariationSettings: '"wght" 130, "slnt" -45',
+            ease: 'linear',
+            duration: 3, 
+        })
+});
 
 
-const logoTl = gsap.timeline({repeat: -1});
-logoTl
-    .set(logo, {
-        fontVariationSettings: '"wght" 130, "slnt" 45',
-    })
-    .to(logo, {
-        fontVariationSettings: '"wght" 130, "slnt" -45',
-        ease: 'linear',
-        duration: 3, 
-    })
 
 
