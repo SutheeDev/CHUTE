9be22e84ab95c864c91914d50c65b9d0f4f7bb2c
SU-T <guitaristpeet@gmail.com>
2022-10-16 14:25:01 -0700

    style span and h1

--- header.js
+++ header.js
@@ -4,25 +4,36 @@ headers.forEach((header) => {
     const logo = header.querySelector('span');
     const logoWidth = logo.clientWidth;
 
-    const h1Tl = gsap.timeline({repeat: -1});
+    for (i = 0; i < 5; i++){
+        header.appendChild(logo.cloneNode(true));
+    }
+ 
+    const h1Tl = gsap.timeline({
+        repeat: -1,
+        yoyo: false,
+    });
     h1Tl
-        .set(header, {x: logoWidth * 1})
+        .set(header, {
+            x: 0,
+            fontVariationSettings: '"wght" 130, "slnt" 45',
+        })
         .to(header, {
             x: logoWidth * -1,
+            fontVariationSettings: '"wght" 130, "slnt" -45',
             ease: 'linear',
             duration: 3,
         })
     
-    const logoTl = gsap.timeline({repeat: -1});
-    logoTl
-        .set(logo, {
-            fontVariationSettings: '"wght" 130, "slnt" 45',
-        })
-        .to(logo, {
-            fontVariationSettings: '"wght" 130, "slnt" -45',
-            ease: 'linear',
-            duration: 3, 
-        })
+    // const logoTl = gsap.timeline({repeat: -1});
+    // logoTl
+    //     .set(logo, {
+    //         fontVariationSettings: '"wght" 130, "slnt" 45',
+    //     })
+    //     .to(logo, {
+    //         fontVariationSettings: '"wght" 130, "slnt" -45',
+    //         ease: 'linear',
+    //         duration: 3, 
+    //     })
 });
 
 
