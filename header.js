4b66f5d45157c52c9a33ad4297a42d56e83fc4b6
SU-T <guitaristpeet@gmail.com>
2022-10-16 22:32:01 -0700

    calculate slant deg

--- header.js
+++ header.js
@@ -1,117 +1,80 @@
-// const headers = document.querySelectorAll('header h1, section h1');
+// const textLines = document.querySelectorAll('header, section');
+// textLines.forEach((line) => {
+//     const header = line.querySelector('h1');
+//     const headerWidth = header.clientWidth;
+//     const spanTags  = header.querySelectorAll('span');
+//     // const spanWidth = spanTag.clientWidth;
+//     console.log(headerWidth);
 
-// headers.forEach((header) => {
-//     const logo = header.querySelector('span');
-//     const logoWidth = logo.clientWidth;
-
-//     for (i = 0; i < 5; i++){
-//         header.appendChild(logo.cloneNode(true));
-//     }
- 
-//     const h1Tl = gsap.timeline({
-//         repeat: -1,
-//         yoyo: false,
-//     });
-//     h1Tl
+//     const headerTl = gsap.timeline();
+//     headerTl
 //         .set(header, {
-//             x: 0,
+//             x: headerWidth,
+//         })
+//         .set(spanTags, {
 //             fontVariationSettings: '"wght" 130, "slnt" 45',
 //         })
 //         .to(header, {
-//             x: logoWidth * -1,
-//             fontVariationSettings: '"wght" 130, "slnt" -45',
+//             x: headerWidth / 4,
+//             // fontVariationSettings: '"wght" 130, "slnt" 45',
+//             duration: 4.5,
 //             ease: 'linear',
-//             duration: 3,
 //         })
-    
-    // const logoTl = gsap.timeline({repeat: -1});
-    // logoTl
-    //     .set(logo, {
-    //         fontVariationSettings: '"wght" 130, "slnt" 45',
-    //     })
-    //     .to(logo, {
-    //         fontVariationSettings: '"wght" 130, "slnt" -45',
-    //         ease: 'linear',
-    //         duration: 3, 
-    //     })
+//         .to(header, {
+//             x: (headerWidth / 4) * -1,
+//             duration: 3,
+//             ease: 'linear',
+//         })
+//         .to(spanTags, {
+//             fontVariationSettings: '"wght" 130, "slnt" -45',
+//             duration: 3,
+//             ease: 'linear',
+//             stagger: {
+//                 amount: 1,
+//             },
+//         }, '<')
+//         .to(header, {
+//             x: headerWidth * -1,
+//             duration: 4.5,
+//             ease: 'linear'
+//         })
 // });
 
-const textLine = document.querySelectorAll('header, section');
+const sections = document.querySelectorAll('header, section');
+sections.forEach(section => {
+    const heading = section.querySelector('h1');
+    const headingWidth = heading.clientWidth;
+    const spanTags = heading.querySelectorAll('span');
 
-
-// const headerTl = gsap.timeline();
-// headerTl
-//     .set(header, {
-//         x: 1000,
-//         fontVariationSettings: '"wght" 130, "slnt" 45',
-//     })
-//     .to(header, {
-//         x: 500,
-//         fontVariationSettings: '"wght" 130, "slnt" 45',
-//         duration: 3,
-//         ease: 'linear',
-//     })
-//     .to(header, {
-//         x: -500,
-//         fontVariationSettings: '"wght" 130, "slnt" -45',
-//         duration: 6,
-//         ease: 'linear',
-//     })
-//     .to(header, {
-//         x: -1000,
-//         fontVariationSettings: '"wght" 130, "slnt" -45',
-//         duration: 3,
-//         ease: 'linear',
-//     })
-
-textLine.forEach((line) => {
-    const header = line.querySelector('h1');
-    const headerWidth = header.clientWidth;
-    // const spanTag  = header.querySelectorAll('span');
-    // const spanWidth = spanTag.clientWidth;
-    console.log(headerWidth);
-
-        const headerTl = gsap.timeline();
-        headerTl
-            .set(header, {
-                // x: '120vw',
-                x: headerWidth,
-            })
-            .to(header, {
-                // x: '-120vw',
-                x: headerWidth * -1,
-                duration: 12,
-                ease: 'linear'
-            })
-
+    spanTags.forEach(spanTag => {
+        let rect = spanTag.getBoundingClientRect();
+        let spanPosition = ((rect.right - rect.left) / 2) + rect.left; 
+        // console.log(spanPosition);
+        let vpWidth = window.innerWidth;
+        let midPoint = vpWidth / 2;
+        let deg = 0;
+        if (spanPosition >= midPoint){
+            let deg = Math.floor(((midPoint / 45) * (spanPosition - midPoint)) * 0.01);
+            spanTag.style.fontVariationSettings = `"wght" 130, "slnt" ${deg}`;
+        } else {
+            let deg = Math.floor((midPoint / 45) * (midPoint - spanPosition) * -0.01);
+            spanTag.style.fontVariationSettings = `"wght" 130, "slnt" ${deg}`;
+        }
+    });
+    
+    const headingTl = gsap.timeline();
+    headingTl
+        .set(heading, {
+            x: 0,
+        })
+        .to(heading, {
+            x: headingWidth * -1,
+            duration: 12,
+            ease: 'linear',
+        })
 });
 
 
-
-// const spanTl = gsap.timeline();
-// spanTl
-//     .set(spanTag, {
-//         fontVariationSettings: '"wght" 130, "slnt" 45',
-//         stagger: 1,
-//     })
-//     .to(spanTag, {
-//         fontVariationSettings: '"wght" 130, "slnt" 45',
-//         duration: 3,
-//         ease: 'linear',
-//         stagger: 1,
-//     })
-//     .to(spanTag, {
-//         fontVariationSettings: '"wght" 130, "slnt" -45',
-//         duration: 6,
-//         ease: 'linear',
-//         stagger: 1,
-//     })
-//     .to(spanTag, {
-//         fontVariationSettings: '"wght" 130, "slnt" -45',
-//         duration: 3,
-//         ease: 'linear',
-//         stagger: 1,
-//     })
 
 
 
